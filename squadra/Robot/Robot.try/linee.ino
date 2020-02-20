a90dfb5202759f2248ccc93705757ce6e2feab9a
ArduSan <58180730+ArduSan@users.noreply.github.com>
2020-02-20 09:20:51 +0100

    Update linee.ino

--- squadra/Robot/Robot.try/linee.ino
+++ squadra/Robot/Robot.try/linee.ino
@@ -1,93 +1,134 @@
+//state of read
 ISR(PCINT2_vect) {
   byte lettura;
   lettura = PINK & 63;
   state = lettura;
   stato = state;
-  if (state != 63) flagsensor = 1;
-  if (state == 63) flagsensor = 0;
-
-}
-
-
-void movsens() {
-  if (state == S8 || state == S13) {
-    vm(255, 200);
-    MuOvItIcAzZo();
-  }
-
-  if (state == S9) {
-    vm(255, 135);
-    MuOvItIcAzZo();
-  }
-  
-  if (state == S10) {
-    vm(255, 10);
-    MuOvItIcAzZo();   
-  }
-  
-  if (state == S11) {
-    vm(255, 290);
-    MuOvItIcAzZo();
-  }
-
-  if (state == S12) {
-    vm(255, 255);
-    MuOvItIcAzZo();
-   
-  }
+  if (state != 63) flg = 1; //se flag sensor è 1 allora vede le linee
+  if (state == 63) flg = 0; //se flag sensor è 0 allora non vede le linee
 }
 
 
 void lemuri(){
   switch(stato){
-    case S8: 
-    vm(255, 200);
+    case S8:
+    G_dir=180;
+    G_speed=Max;
+    //vm(255, 180);
     break;
     
     case S13:
-    vm(255, 200);
+    G_dir=180;
+    G_speed=Max;
+    //vm(255, 180);
     break;
-
+    
     case S9:
-    vm(255,135);
+    G_dir=90;
+    G_speed=Max;
+    //vm(255,90);
     break;
 
     case S10:
-    vm(255,10);
+    G_dir=0;
+    G_speed=Max;
+    //vm(255,0);
     break;
 
     case S11:
-    vm(255,349);
+    G_dir=0;
+    G_speed=Max;
+    //vm(255,0)
     break;
 
     case S12:
-    vm(255,255);
+    G_dir=270;
+    G_speed=Max;
+    //vm(255,270);
     break;
+      
   }
-}
-
-
-/*void sPoStAtIcAzZo() {
-  if (flagsensor == 1) {
-    for(int k=0;k<500;k++){
-      if(k<=499) lemuri();
-      if(k>499){
-        ganon(55);
-        k=0;
-      }
-    }
-  }
-}
-*/
-
-void cAzZoSpOsTaTi(){
   
 }
 
-void MuOvItIcAzZo() {
-  if (flagsensor == 0) ganon(55);
+
+void morcock(){
+  if(flg==1){
+    if(stopT==0)    stopT=millis()+100,Serial.println("start");
+  }    
+     if(stopT>millis()){
+      Serial.println("freno");
+      stopm();
+    }
+    if(stopT!=0 && stopT<millis()){
+      Serial.println("restart");
+      stopT=0;
+      }
+  }
+  /*if(sotpT>millis() && flg==0){
+    folk[0];
+      folk[1];
+      folk[2];
+  }
+  if(stopT<millis()){
+    if(flg==1){
+      stopT2=millis()+200;
+      if(stopT2>millis()) lemuri();
+}      
+   else if(stopT2>millis()&&flg==0) lemuri();
+   else if(stopT2<millis()&&flg==0) {
+    ganon(55);
+    stopT=0; 
+   }
+  }
+}*/
+
+
+void morcock2() {
+int stopF;
+  
+  if(flg==1){
+    stopF=millis()+50;
+    stopm();  
+  }
+
+  if(flg==1 && stopF>millis()){
+    stopT=millis()+100;
+    lemuri();
+  }
+
+  if(flg==0 && stopT>millis()) lemuri();
+
+  if(flg==0 && stopT<millis()) ganon(55);
+  
 }
 
-void pArAcAzZo(){  
-  if(flagsensor==0)gengar();
-}
+
+/*void sveltina(){
+   
+  if(flg==1){
+    stopT=millis()+100;
+    if(stopT>millis())lemuri();
+  }
+  
+   else if(stopT>millis()&&flg==0) lemuri();
+   else if(stopT<millis()&&flg==0) ganon(55);
+      
+}*/
+    
+
+
+//non uscire dalle line porca madonna
+
+
+/*void ahah(){
+  
+  while(flg==1){
+  lemuri();
+  stopT=millis()+200;
+  }
+  if(stopT<millis()){
+    if(flg==0)ganon(55);
+  }
+}*/
+ 
