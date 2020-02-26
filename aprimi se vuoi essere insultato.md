2d0fc23107423d8b4f256f7c804bed6a34da5227
Simone <ferretti2003simone@gmail.com>
2020-02-26 13:23:58 +0100

    yo

--- aprimi se vuoi essere insultato.md
+++
@@ -1,2 +0,0 @@
-scemo chi legge
-
--- squadra/Robot/Robot.try/bussola.ino
+++
@@ -1,7 +0,0 @@
-void readimu() {
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  spompaciuccioapalloncino = euler.x();
-  /*Serial.println(spompaciuccioapalloncino);
-  delay(250);
-  */
-}
