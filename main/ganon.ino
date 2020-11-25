4ac9c8388744a3829912ed7fbd6007b7739b9dae
Mikilus3 <michele.giura@hotmail.com>
2020-11-25 18:24:32 +0100

    aggiornato

--- main/ganon.ino
+++ main/ganon.ino
@@ -1,49 +1,53 @@
-void ganon(int plus) {
+//FUNZIONE ATTACCANTE 
+
+void ganon(int plus) {                       //vado a inizializzare la funzione ganon
   spi_readfrom644l();
   int dir = 0;
   int ball2;
   int ang = ang_sen[ball_sensor];
+  
+//VADO A DEFINIRE PER OGNI GRUPPO DI SENSORI CHE VEDE LA PALLA UN ANGOLO DI MOVIMENTO SPECIFICO
 
   if(flg == 0) {
-  if (ang >= 340 || ang <= 20) {
+  if (ang >= 340 || ang <= 20){              //qua si va a definire l'angolo in un range di >=340 <=20
     G_dir=ang;
     G_speed=Max;
     
   }
-    if (ang > 20 && ang <= 80)  {
+    if (ang > 20 && ang <= 80){              //qua si va a definire l'angolo in un range di >20 <=80
       G_dir=ang+30;
       G_speed=Max;
       
     }
-    if (ang < 340 && ang >= 280){
+    if (ang < 340 && ang >= 280){            //qua si va a definire l'angolo in un range di <340 >=280
       G_dir=ang-30;
       G_speed=Max;
       
     }
 
-    if (ang > 80 && ang <= 150){
+    if (ang > 80 && ang <= 150){             //qua si va a definire l'angolo in un range di >80 <=150
       G_dir=180;
       G_speed=Max;
       
     }
-    if (ang < 280 && ang >= 210)  {
+    if (ang < 280 && ang >= 210){            //qua si va a definire l'angolo in un range di <280 >=210
       G_dir=180;
       G_speed=Max;
       
     }
 
-    if (ang > 150 && ang < 210) {
+    if (ang > 150 && ang < 210){             //qua si va a definire l'angolo in un range di >150 <210
       G_dir=140;
       G_speed=Max;
       
     }
 
-    if(ball_distance == 6) {
+    if(ball_distance == 6){                  //se i sensori vedono la palla troppo distante, il movimento si blocca
       stopm();
       
     }
   }
   
-  vm(G_speed,G_dir);//assegno il movimento
+  vm(G_speed,G_dir);                         //assegno il movimento
   
 }
--- main/mv.ino
+++ main/mv.ino
@@ -1,29 +1,31 @@
-// verso orario
- void orario(int mot ) { // funzione movimento orario
+//VERSO ORARIO
+ void orario(int mot ) {                        // funzione movimento orario
   digitalWrite(A[mot], HIGH);
   digitalWrite(B[mot], LOW);
 }
 
 
-//clockwise verse
-void antiorario(int mot) { // funzione movimento antiorario
+//VERSO ANTIORARIO
+void antiorario(int mot) {                      // funzione movimento antiorario
   digitalWrite(A[mot], LOW);
   digitalWrite(B[mot], HIGH);
 }
 
-// stop
-void heartattack(int mot) {
+//STOP
+/*void heartattack(int mot) {                   //funzione di riserva di stopm()
   digitalWrite(A[mot], HIGH);
   digitalWrite(B[mot], HIGH);
   analogWrite(PWM[mot], 235);
-}
+}*/
+
+//FOLLE
 void folk(int mot){
-    digitalWrite(A[mot], LOW);
+  digitalWrite(A[mot], LOW);                    //funzione di folle dei motori
   digitalWrite(B[mot], LOW);
   analogWrite(PWM[mot], 0);
 }
 
-void stopm(){
+void stopm(){                                   //funzione di stop dei motori
   G_dir=0;
   G_speed=0;
   /*folk(0);
@@ -31,31 +33,33 @@ void stopm(){
   folk(2);*/
   
 }
-void vm(float V, float d) // funzione movimento
+void vm(float V, float d)                                                  //funzione di movimento
 {
-  vx = V * cos(rad(d)); //vettore asse x
-  vy = -V * sin(rad(d)); //vettore asse y
+  vx = V * cos(rad(d));                                                    //vettore asse x
+  vy = -V * sin(rad(d));                                                   //vettore asse y
 
 
 
-  velocita1 = sin(rad(60)) * vx + cos (rad(60)) * vy; //vmot1
-  velocita2 = sin(rad(180)) * vx + cos (rad(180)) * vy; //vmot2
-  velocita3 = sin(rad(300)) * vx + cos (rad(300)) * vy; //vmot3
+  velocita1 = sin(rad(60)) * vx + cos (rad(60)) * vy;                      //velocità motore 1
+  velocita2 = sin(rad(180)) * vx + cos (rad(180)) * vy;                    //velocità motore 2
+  velocita3 = sin(rad(300)) * vx + cos (rad(300)) * vy;                    //velocità motore 3
 
 
-  pidfactor = upid(); // aggiorno il pid con l'upid
+  pidfactor = upid();                                                      //aggiorno il pid con l'upid
 
-  velocita1 += pidfactor;
-  velocita2 += pidfactor;
-  velocita3 += pidfactor;
+  velocita1 += pidfactor;                                                  //aggiungo il pidfactor alla velocità del motore 1
+  velocita2 += pidfactor;                                                  //aggiungo il pidfactor alla velocità del motore 2
+  velocita3 += pidfactor;                                                  //aggiungo il pidfactor alla velocità del motore 3
 
-  velocita1 = constrain(velocita1, -235 , +235); //do alle velocita il comando che non possono superare il 255 e non può andare sotto al -255
+  //do alle velocita il comando che non possono superare il 255 e non può andare sotto al -255
+  velocita1 = constrain(velocita1, -235 , +235);                           
   velocita2 = constrain(velocita2, -235 , +235);
   velocita3 = constrain(velocita3, -235 , +235);
 
+  //abs sta per prendo solo i valori assoluti(positivi)
   if (velocita1 > 0) orario(0);
   else antiorario(0);
-  analogWrite(PWM[0], abs(velocita1)); //abs sta per prendo solo i valori assoluti(positivi)
+  analogWrite(PWM[0], abs(velocita1));  
 
   if (velocita2 > 0) orario(1);
   else antiorario(1);
